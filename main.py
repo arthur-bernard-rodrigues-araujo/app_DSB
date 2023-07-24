57f019051babbda6a6bc93e0342b34e76353cd16
arthur-bernard-rodrigues-araujo <139857468+arthur-bernard-rodrigues-araujo@users.noreply.github.com>
2023-07-24 14:18:57 -0300

    Update main.py
    atualização de layout do app, agora com 2 quadrantes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
@@ -49,37 +50,97 @@ london_neighborhoods = [
 app.layout = html.Div(
     style={
         'textAlign': 'center',
-        'background': '#ffffff',  # Background branco
+        'background': '#ffffff',
         'height': '100vh',
         'display': 'flex',
         'flexDirection': 'column',
         'alignItems': 'center',
-        'justifyContent': 'center',
-        'fontFamily': 'Arial',  # Fonte Arial
+        'fontFamily': 'Arial',
     },
     children=[
-        html.H1("London Housing Prices Calculator", style={'marginBottom': '20px', 'fontSize': '36px', 'fontFamily': 'Arial'}),
-        html.Label("Select a neighborhood:", style={'fontSize': '18px', 'fontFamily': 'Arial'}),
-        dcc.Dropdown(
-            id='neighborhood-dropdown',
-            options=[{'label': neighborhood, 'value': neighborhood} for neighborhood in london_neighborhoods],
-            value=london_neighborhoods[0],
-            style={'width': '300px', 'height': '50px', 'marginBottom': '10px', 'fontSize': '16px', 'fontFamily': 'Arial'},
+        html.H1(
+            "London Housing Prices Calculator",
+            style={'marginTop': '20px', 'marginBottom': '20px', 'fontSize': '36px', 'fontFamily': 'Arial'}
         ),
-        html.Label("Property Area (m²):", style={'fontSize': '18px', 'fontFamily': 'Arial'}),
-        dcc.Input(id='area-input', type='number', placeholder='Property Area (m²)', value=1455, style={'width': '300px', 'height': '50px', 'marginBottom': '10px', 'fontSize': '16px', 'fontFamily': 'Arial', 'textAlign': 'center'}),
-        html.Label("Distance to Tube (m):", style={'fontSize': '18px', 'fontFamily': 'Arial'}),
-        dcc.Input(id='distance-input', type='number', placeholder='Distance to Tube (m)', value=949, style={'width': '300px', 'height': '50px', 'marginBottom': '10px', 'fontSize': '16px', 'fontFamily': 'Arial', 'textAlign': 'center'}),
-        html.Label("Number of Bathrooms:", style={'fontSize': '18px', 'fontFamily': 'Arial'}),
-        dcc.Input(id='bathrooms-input', type='number', placeholder='Number of Bathrooms', value=1, style={'width': '300px', 'height': '50px', 'marginBottom': '30px', 'fontSize': '16px', 'fontFamily': 'Arial', 'textAlign': 'center'}),
-        html.Button('Calculate Price', id='button', n_clicks=0, style={'textAlign': 'center', 'width': '300px', 'height': '50px', 'fontSize': '18px', 'fontFamily': 'Arial'}),
-        html.Div(id='output', style={'marginTop': '30px', 'fontSize': '20px', 'fontFamily': 'Arial'})
+        html.Div(
+            style={'display': 'flex', 'flexDirection': 'row', 'width': '80%'},
+            children=[
+                html.Div(
+                    style={'flex': '1', 'padding': '20px'},
+                    children=[
+                        html.Label(
+                            "Select a neighborhood:",
+                            style={'fontSize': '18px', 'fontFamily': 'Arial', 'fontWeight': 'bold'}
+                        ),
+                        dcc.Dropdown(
+                            id='neighborhood-dropdown',
+                            options=[{'label': neighborhood, 'value': neighborhood} for neighborhood in london_neighborhoods],
+                            value=london_neighborhoods[0],
+                            style={'width': '100%', 'height': '50px', 'marginBottom': '10px', 'fontSize': '16px', 'fontFamily': 'Arial'},
+                        ),
+                        html.Label(
+                            "Property Area (m²):",
+                            style={'fontSize': '18px', 'fontFamily': 'Arial', 'fontWeight': 'bold'},
+                        ),
+                        dcc.Input(
+                            id='area-input',
+                            type='number',
+                            placeholder='Property Area (m²)',
+                            value=1455,
+                            style={'width': '100%', 'height': '50px', 'marginBottom': '10px', 'fontSize': '16px', 'fontFamily': 'Arial', 'textAlign': 'center'},
+                        ),
+                        html.Label(
+                            "Distance to Tube (m):",
+                            style={'fontSize': '18px', 'fontFamily': 'Arial', 'fontWeight': 'bold'}
+                        ),
+                        dcc.Input(
+                            id='distance-input',
+                            type='number',
+                            placeholder='Distance to Tube (m)',
+                            value=949,
+                            style={'width': '100%', 'height': '50px', 'marginBottom': '10px', 'fontSize': '16px', 'fontFamily': 'Arial', 'textAlign': 'center'},
+                        ),
+                        html.Label(
+                            "Number of Bathrooms:",
+                            style={'fontSize': '18px', 'fontFamily': 'Arial', 'fontWeight': 'bold'}
+                        ),
+                        dcc.Input(
+                            id='bathrooms-input',
+                            type='number',
+                            placeholder='Number of Bathrooms',
+                            value=1,
+                            style={'width': '100%', 'height': '50px', 'marginBottom': '30px', 'fontSize': '16px', 'fontFamily': 'Arial', 'textAlign': 'center'},
+                        ),
+                        html.Button(
+                            'Calculate Price',
+                            id='button',
+                            n_clicks=0,
+                            style={'textAlign': 'center', 'width': '100%', 'height': '50px', 'fontSize': '18px', 'fontFamily': 'Arial'}
+                        ),
+                    ]
+                ),
+                html.Div(
+                    style={'flex': '1', 'padding': '20px', 'fontSize': '20px', 'fontFamily': 'Arial', 'textAlign': 'center'},
+                    children=[
+                        html.Div(
+                            style={'marginTop': '20px', 'fontWeight': 'bold'},
+                            id='output-value-container'
+                        ),
+                        html.Div(
+                            style={'marginTop': '30px', 'fontSize': '18px', 'fontFamily': 'Arial'},
+                            id='price-container'
+                        )
+                    ]
+                )
+            ]
+        ),
     ]
 )
 
 # Callback function to update the output div
 @app.callback(
-    Output('output', 'children'),
+    [Output('output-value-container', 'children'),
+     Output('price-container', 'children')],
     [Input('button', 'n_clicks')],
     [dash.dependencies.State('neighborhood-dropdown', 'value'),
      dash.dependencies.State('area-input', 'value'),
@@ -87,30 +148,49 @@ app.layout = html.Div(
      dash.dependencies.State('bathrooms-input', 'value')]
 )
 def update_output(n_clicks, neighborhood, area, distance, bathrooms):
-    if n_clicks > 0:
-        if neighborhood is None or area is None or distance is None or bathrooms is None:
-            return "Please fill in all fields."
-        else:
-            # Convert area and distance to meters if needed (assuming they are initially provided in meters)
-            area_m2 = int(area) if int(area) > 0 else 0
-            distance_m = int(distance) if int(distance) > 0 else 0
-            bathrooms = int(bathrooms) if int(bathrooms) > 0 else 0
+    if neighborhood is not None:
+        neighborhood_text = neighborhood
+    else:
+        neighborhood_text = ""
 
-            data = pd.read_excel(url, engine='openpyxl')
-            # Filter the data based on the selected neighborhood, area, distance, and bathrooms
-            filtered_data = data[(data['neighborhood'] == neighborhood) & (data['area'] == area_m2) & (
-                        data['distance'] == distance_m) & (data['bathrooms'] == bathrooms)]
+    if n_clicks > 0 and area is not None and distance is not None and bathrooms is not None:
+        # Convert area and distance to meters if needed (assuming they are initially provided in meters)
+        area_m2 = int(area) if int(area) > 0 else 0
+        distance_m = int(distance) if int(distance) > 0 else 0
+        bathrooms = int(bathrooms) if int(bathrooms) > 0 else 0
 
-            if filtered_data.empty:
-                return "No data available for the selected criteria."
-            else:
-                # Get the estimated price for the selected combination
-                estimated_price = filtered_data['estimated_price'].values[0]
+        data = pd.read_excel(url, engine='openpyxl')
+        # Filter the data based on the selected neighborhood, area, distance, and bathrooms
+        filtered_data = data[(data['neighborhood'] == neighborhood) & (data['area'] == area_m2) & (
+                    data['distance'] == distance_m) & (data['bathrooms'] == bathrooms)]
 
-                # Format the estimated price as a monetary value (pounds)
-                formatted_price = f'£{estimated_price:,.2f}'  # Adiciona o símbolo de libra esterlina e vírgula de milhar
+        if filtered_data.empty:
+            return "No data available for the selected criteria.", ""
 
-                return f"The estimated price for {neighborhood} is: {formatted_price}"
+        # Get the estimated price for the selected combination
+        estimated_price = filtered_data['estimated_price'].values[0]
+
+        # Calculate future price (estimated price * 2)
+        future_price = estimated_price * 2
+
+        # Format the estimated price and future price as monetary values (pounds)
+        formatted_price = f'£{estimated_price:,.2f}'
+        formatted_future_price = f'£{future_price:,.2f}'
+
+        # Set the color based on the value
+        color = 'red' if estimated_price > 1000000 else 'green'
+
+        # Prepare the output text
+        output_text = f"The estimated price for {neighborhood_text} is: {formatted_price}"
+
+        # Combine both prices into a single text with different colors for estimated and future prices
+        price_text = html.Div([
+            "The estimated property price for the next 5 years is: ", html.Span(formatted_future_price, style={'color': color})
+        ])
+
+        return output_text, price_text
+    else:
+        return "Please fill in all fields.", ""
 
 if __name__ == '__main__':
     app.run_server(debug=True)
